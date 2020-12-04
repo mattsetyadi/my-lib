b0b5a3ddff1a0ccb70ffb4a27a6197fc4133811a
mattsetyadi <matt.musicianstuff@gmail.com>
2020-12-04 15:46:28 +0700

    add

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
 const authorRouter = require('./routes/authors');
@@ -14,6 +15,7 @@ app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, {

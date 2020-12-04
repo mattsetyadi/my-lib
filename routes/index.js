2f1c7da0e6a48a4df19810763040d470f43e80f0
mattsetyadi <matt.musicianstuff@gmail.com>
2020-12-04 18:55:19 +0700

    book section

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const book = require('../models/book');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('index');
+router.get('/', async (req, res) => {
+  let books;
+  try {
+    books = await book.find().sort({ createdAt: 'desc' }).limit(10).exec();
+  } catch {
+    books = [];
+  }
+  res.render('index', { books: books });
 });
 
 module.exports = router;
